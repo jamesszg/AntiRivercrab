4d4453e07dc1a5948325e595608d22ae6ddb1547
xxzl0130 <zwx0130@buaa.edu.cn>
2019-09-03 23:27:27 +0800

    debug and build

---
+++ build_mac.bat
@@ -0,0 +1,3 @@
+set GOARCH=amd64
+set GOOS=darwin
+go build -o AntiRivercrab .\main.go
--- main.go
+++ main.go
@@ -5,78 +5,118 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net"
+	"fmt"
+	"time"
 	"net/http"
 	"strings"
+	"regexp"
 
 	"github.com/elazarl/goproxy"
-	log "github.com/xxzl0130/AntiRivercrab/pkg/log"
+	"github.com/pkg/errors"
+	log_std "github.com/xxzl0130/AntiRivercrab/pkg/log"
 	"github.com/xxzl0130/AntiRivercrab/pkg/cipher"
 	"github.com/xxzl0130/AntiRivercrab/pkg/util"
 )
 
-var sign string
+type AntiRivercrab struct {
+	log  log_std.Logger
+	sign string
+	pattern string
+	replacement string
+}
 
 func main() {
-	localhost, err := getLocalhost()
+	log, err := log_std.New(fmt.Sprintf("AntiRivercrab.%d.log", time.Now().Unix()))
 	if err != nil {
-		log.Fatalf("获取代理地址失败 -> %+v", err)
+		panic(fmt.Sprintf("%+v", err))
 	}
 
-	log.Tipsf("代理地址 -> %s:%d", localhost, 8888)
-
-	srv := goproxy.NewProxyHttpServer()
-	srv.Logger = new(util.NilLogger)
-	srv.OnResponse(condition()).DoFunc(onResponse)
-
-	if err := http.ListenAndServe(":8888", srv); err != nil {
-		log.Fatalf("启动代理服务器失败 -> %+v", err)
+	ar := &AntiRivercrab{
+		log:  log,
+		sign : "",
+		pattern : "\"naive_build_gun_formula\":\"(\\d+:\\d+:\\d+:\\d+)?\"",
+		replacement : "\"naive_build_gun_formula\":\"33:33:33:33\"",
+	}
+	if err := ar.Run(); err != nil {
+		ar.log.Fatalf("程序启动失败 -> %+v", err)
 	}
 }
 
-func onResponse(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+func (ar *AntiRivercrab) Run() error {
+	localhost, err := ar.getLocalhost()
+	if err != nil {
+		ar.log.Fatalf("获取代理地址失败 -> %+v", err)
+	}
+
+	ar.log.Tipsf("代理地址 -> %s:%d", localhost, 8888)
+
+	srv := goproxy.NewProxyHttpServer()
+	srv.Logger = new(util.NilLogger)
+	srv.OnResponse(ar.condition()).DoFunc(ar.onResponse)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", 8888), srv); err != nil {
+		ar.log.Fatalf("启动代理服务器失败 -> %+v", err)
+	}
+
+	return nil
+}
+
+func (ar *AntiRivercrab) onResponse(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 	type Uid struct {
 		Sign            string `json:"sign"`
 	}
 
-	log.Infof("处理请求响应 -> %s", path(ctx.Req))
+	ar.log.Infof("处理请求响应 -> %s", path(ctx.Req))
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Errorf("读取响应数据失败 -> %+v", err)
+		ar.log.Errorf("读取响应数据失败 -> %+v", err)
+		resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		return resp
 	}
 	if strings.HasSuffix(ctx.Req.URL.Path,"/Index/getDigitalSkyNbUid"){
 		data, err := cipher.AuthCodeDecodeB64(string(body)[1:], "yundoudou", true)
 		if err != nil {
-			log.Errorf("解析Uid数据失败 -> %+v", err)
+			ar.log.Errorf("解析Uid数据失败 -> %+v", err)
+			resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 			return resp
 		}
 		uid := Uid{}
 		if err := json.Unmarshal([]byte(data), &uid); err != nil {
-			log.Errorf("解析JSON数据失败 -> %+v", err)
+			ar.log.Errorf("解析JSON数据失败 -> %+v", err)
+			resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 			return resp
 		}
-		sign = uid.Sign
+		ar.sign = uid.Sign
+		ar.log.Infof("解析Uid成功")
+		resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		return resp
 	} else if strings.HasSuffix(ctx.Req.URL.Path,"/Index/index"){
-		data, err := cipher.AuthCodeDecodeB64(string(body)[1:], sign, true)
+		data, err := cipher.AuthCodeDecodeB64(string(body)[1:], ar.sign, true)
 		if err != nil {
-			log.Errorf("解析用户数据失败 -> %+v", err)
+			ar.log.Errorf("解析用户数据失败 -> %+v", err)
+			resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 			return resp
 		}
-		body = []byte(strings.Replace(data,"\"naive_build_gun_formula\": \"\"","\"naive_build_gun_formula\":\"33:33:33:33\"",1))
+		ar.log.Infof("解析用户数据成功")
+		body = []byte(regexp.MustCompile(ar.pattern).ReplaceAll([]byte(data), []byte(ar.replacement)))
+		if(err != nil){
+			ar.log.Errorf("打包用户数据失败 -> %+v", err)
+			resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+			return resp
+		}
+		ar.log.Tipsf("AntiRivercrab行动成功")
 	}
 	resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
 	return resp
 }
 
-func condition() goproxy.ReqConditionFunc {
+func (ar *AntiRivercrab) condition() goproxy.ReqConditionFunc {
 	return func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
-		log.Infof("请求 -> %s", path(req))
-		if strings.HasSuffix(req.Host, "ppgame.com") || strings.HasSuffix(req.Host, "sn-game.txwy.tw") {
+		ar.log.Infof("请求 -> %s", path(req))
+		if strings.HasSuffix(req.Host, "ppgame.com") {
 			if strings.HasSuffix(req.URL.Path, "/Index/index") || strings.HasSuffix(req.URL.Path, "/Index/getDigitalSkyNbUid"){
-				log.Infof("请求通过 -> %s", path(req))
+				ar.log.Infof("请求通过 -> %s", path(req))
 				return true
 			}
 		}
@@ -84,7 +124,7 @@ func condition() goproxy.ReqConditionFunc {
 	}
 }
 
-func getLocalhost() (string, error) {
+func (ar *AntiRivercrab) getLocalhost() (string, error) {
 	conn, err := net.Dial("tcp", "www.baidu.com:80")
 	if err != nil {
 		return "", errors.WithMessage(err, "连接 www.baidu.com:80 失败")
